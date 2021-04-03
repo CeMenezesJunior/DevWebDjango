ed97f1b12a3e09cbde62e72464b5fc0152ac42cf
Carlos Eduardo Menezes Junior <carloseduardojunior@id.uff.br>
2021-04-03 11:37:07 -0300

    Ajustes de tela e banco de dados

--- rockstreet/album/models.py
+++ rockstreet/album/models.py
@@ -8,6 +8,7 @@ class Album(models.Model):
     artista = models.ForeignKey(Artista,related_name='albuns',on_delete=models.DO_NOTHING)
     nome = models.CharField(max_length=70,db_index=True,unique=True)
     capa = models.CharField(max_length=150)
+    link_spotify = models.CharField(max_length=100)
     slug = models.SlugField(max_length=70)
 
     class Meta:
